0dcd55aea11cb15aabf27f0a55d06ad002507256
mohkhald <mohkhald@student.1337.ma>
2026-05-14 22:08:38 +0100

    cpp project/practice

--- cpp_practices/educative/Account.cpp
+++ cpp_practices/educative/Account.cpp
@@ -1,60 +1,61 @@
 #include <iostream>
 using namespace std;
 
+#include <iostream>
+using namespace std;
+
 // Write classes code here
 class Account {
   protected:
   float balance;
   
   public:
-  void Withdraw(float amount);
-  void Deposit(float amount);
-  void printBalance();
+  Account(float b) {balance = b;}
+  
+ virtual void Withdraw(float amount) {}
+ virtual void Deposit(float amount) {}
+ virtual void printBalance() {}
 };
+
 // make base class functions virtual
 
 class Savings: public Account {
   private:
-  float interest_rate;
+  float interest_rate = 0.8;
   
   public:
-  void Withdraw(float amount);
-  void Deposit(float amount);
-  void printBalance();
+  Savings(int bal): Account(bal) {}
+  void Withdraw(float amount) {balance -= amount + (amount * interest_rate);}
+  void Deposit(float amount) {balance += amount + (amount * interest_rate);}
+  void printBalance() {cout<<"Balance in your saving account: "<<balance<<endl;}
 };
 
 class Current: public Account {
-  private:
-  float interest_rate;
-  
   public:
-  void Withdraw(float amount);
-  void Deposit(float amount);
-  void printBalance();
+  Current(float bal): Account(bal) {}
+  void Withdraw(float amount){balance -= amount;}
+  void Deposit(float amount){balance += amount;}
+  void printBalance(){cout<<"Balance in your current account: "<<balance<<endl;}
 };
 
-Savings::Savings(float b, float ir): {Account::balance(b);ir=0.8;}
-
-Current::Current(float b): Account::balance(b),  {}
-
 int main() {
-  // make instances of classes here
-  // call their traits functions here
+  // creating savings account object
   Savings s1(50000);
-  Account * acc = &s1;
+  Account * acc = &s1;   // pointing acc to savings object
   acc->Deposit(1000);
   acc->printBalance();
   
   acc->Withdraw(3000);
   acc->printBalance();
   
+  cout << endl;
   
+  // creating current account object
   Current c1(50000);
-  acc = &c1;
+  acc = &c1;     //// pointing acc to current object
   acc->Deposit(1000);
   acc->printBalance();
   
   acc->Withdraw(3000);
-  acc->printBalance(); 
-  return 0;
+  acc->printBalance();  
 }
